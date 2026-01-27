4210de9b43ca5357f4d0f5c2956184e155d2a138
Jesus <alonsoas.guerrero@gmail.com>
2026-01-27 01:25:03 -0500

    feat: Add gh-pages deployment and fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminUsers from './pages/AdminUsers';
 import AdminRoute from './components/AdminRoute';
 
